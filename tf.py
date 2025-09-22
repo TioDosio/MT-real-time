8d2b341fe08d8d2a7dcc785eec488f78445391a7
andre <andre.teodosio@tecnico.ulisboa.pt>
2025-09-22 14:12:07 +0100

    listens for another transformationa and publish them

--- tf.py
+++ tf.py
@@ -2,29 +2,49 @@
 import rospy
 import tf2_ros
 from geometry_msgs.msg import TransformStamped
+from tf2_msgs.msg import TFMessage
 
 def main():
     rospy.init_node('tf_publisher')
     tf_buffer = tf2_ros.Buffer()
     tf_listener = tf2_ros.TransformListener(tf_buffer)
-    tf_pub = rospy.Publisher('/robot_tf', TransformStamped, queue_size=10)
+    tf_pub = rospy.Publisher('/combined_tf', TFMessage, queue_size=10)
     rate = rospy.Rate(50)  # 50 Hz
 
     rospy.loginfo("TF publisher node started")
 
     while not rospy.is_shutdown():
+        transforms = []
+        
         try:
             # Look up the transform from base_footprint to odom (robot's position in world)
             # This gives you the robot's pose relative to the odometry frame
-            trans = tf_buffer.lookup_transform('odom', 'base_footprint', rospy.Time(0), rospy.Duration(0.1))
-
-            # Publish the transform
-            tf_pub.publish(trans)
+            robot_trans = tf_buffer.lookup_transform('odom', 'base_footprint', rospy.Time(0), rospy.Duration(0.1))
+            robot_trans.child_frame_id = "base_footprint"  # Add identifier
+            transforms.append(robot_trans)
             
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
-            rospy.logdebug("TF lookup error: %s" % str(e))
+            rospy.logdebug("Robot TF lookup error: %s" % str(e))
         except Exception as e:
-            rospy.logerr("Unexpected error: %s" % str(e))
+            rospy.logerr("Unexpected error in robot TF: %s" % str(e))
+        
+        try:
+            # Look up the transform from odom to camera_link (camera's position in world)
+            # This gives you the camera's pose relative to the odometry frame
+            camera_trans = tf_buffer.lookup_transform('odom', 'l_eye_link', rospy.Time(0), rospy.Duration(0.1))
+            camera_trans.child_frame_id = "l_eye_link"  # Add identifier
+            transforms.append(camera_trans)
+            
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+            rospy.logdebug("Camera TF lookup error: %s" % str(e))
+        except Exception as e:
+            rospy.logerr("Unexpected error in camera TF: %s" % str(e))
+        
+        # Publish both transforms in a single message if we have any
+        if transforms:
+            tf_message = TFMessage()
+            tf_message.transforms = transforms
+            tf_pub.publish(tf_message)
         
         rate.sleep()
 
